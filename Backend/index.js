13c706f3a6e37eb0780728c7a0919b4d77b5dd80
Ananya Sharma <ananyasharma3064@gmail.com>
2025-08-16 23:32:23 -0400

    first http request

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import {PORT} from "./config.js"
 const app =express(); 
+app.get('/',(req,res)=>{
+    console.log(req)
+    return response.status(200).send("welcome to MERN stack")
+})
 app.listen(PORT,()=>{
     console.log(`app is listening on port:${PORT}`);
 })
