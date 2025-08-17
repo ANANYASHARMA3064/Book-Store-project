f7ca659b20e35f40635eb84c73b968d24578c05d
Ananya Sharma <ananyasharma3064@gmail.com>
2025-08-17 19:07:12 -0400

    Remove backend/config.js from repo

--- Backend/config.js
+++
@@ -1,3 +0,0 @@
-export const PORT= 8090; 
-encodeURIComponent("")
-export const mongoDBURL = "mongodb+srv://ananyasharma3064:Voda%4028112006@mern-bookstore.hbsowb7.mongodb.net/bookstore?retryWrites=true&w=majority&appName=MERN-bookstore";
