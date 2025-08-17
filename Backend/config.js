4a8ec4198a8333bf5294b1f3f5a64e3cbd539848
Ananya Sharma <ananyasharma3064@gmail.com>
2025-08-17 19:02:01 -0400

    connected to mongoDB

--- Backend/config.js
+++ Backend/config.js
@@ -1,1 +1,3 @@
 export const PORT= 8090; 
+encodeURIComponent("")
+export const mongoDBURL = "mongodb+srv://ananyasharma3064:Voda%4028112006@mern-bookstore.hbsowb7.mongodb.net/bookstore?retryWrites=true&w=majority&appName=MERN-bookstore";
--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,17 @@
 import express from "express";
-import {PORT} from "./config.js"
+import {PORT,mongoDBURL} from "./config.js"
+import mongoose from "mongoose";
 const app =express(); 
 app.get('/',(req,res)=>{
     console.log(req)
     return response.status(200).send("welcome to MERN stack")
 })
-app.listen(PORT,()=>{
+
+mongoose.connect(mongoDBURL).then(()=>{
+    console.log('app connected to database')
+    app.listen(PORT,()=>{
     console.log(`app is listening on port:${PORT}`);
+})
+}).catch((error)=>{
+    console.log(error)
 })
