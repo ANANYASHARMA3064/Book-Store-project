d318667c01e3e11c3e2603a1e581b0df99fb4acd
Ananya Sharma <ananyasharma3064@gmail.com>
2025-08-18 23:26:31 -0400

    save a book

--- Backend/index.js
+++ Backend/index.js
@@ -6,7 +6,36 @@ app.get('/',(req,res)=>{
     console.log(req)
     return response.status(200).send("welcome to MERN stack")
 })
+// Route for saving a new book 
+app.post('/books',async (request,response)=>{
+    try{
+        if(
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ){
+            return response.status(400).send({
+                message: "send all required fields"
+            })
+        }
+        
+    }
+    catch(error){
+        console.log(error.message);
+        response.status(500).send({message:error.message})
+    }
+    const newBook={
+        title:request.body.title,
+        author:request.body.author,
+        publishYear:request.body.publishYear
+    }
+    catch(error){
+        console.log(error.message);
+        response.status(500).send({message:error.message})
+    }
+    
 
+})
 mongoose.connect(mongoDBURL).then(()=>{
     console.log('app connected to database')
     app.listen(PORT,()=>{
