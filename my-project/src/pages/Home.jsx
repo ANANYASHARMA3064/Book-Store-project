7020dbbcfbc66f275770780ae02003da9ffdc927
Ananya Sharma <ananyasharma3064@gmail.com>
2025-08-25 23:32:05 -0400

    react-show books part-1

---
+++ my-project/src/pages/Home.jsx
@@ -0,0 +1,35 @@
+import { useEffect,useState } from "react"
+import axios from "axios"
+import Spinner from "../Components/Spinner"
+import { Link } from "react-router-dom"
+import { AiOutlineEdit } from "react-icons/ai"
+import { BsInfoCircle } from "react-icons/bs"
+import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md"
+ 
+const Home = () => {
+    const [books,setBooks]=useState([]);
+    const [loading,setLoading]=usesState(false)
+    useEffect(()=>{
+        setLoading(true)
+        axios.get('http://localhost:8090/books')
+        .then((response)=>{
+            setBooks(response.data.data)
+            setLoading(false)
+        })
+        .catch((error)=>{
+            console.log(error) 
+            setLoading(false)
+        })
+    },[])
+  return (
+    <div className="p-4">
+        <div className="flex justify-between items-center">
+            <h1 className="text-3xl m-8">BOOK LIST </h1>
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default Home
